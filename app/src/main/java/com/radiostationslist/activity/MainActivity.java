8bfdd24b69b20d4dc1cb25b17632588234a6fd7c
CGTDhananjay <dhananjay.mohnot@cgt.co.in>
2015-12-15 20:23:02 +0530

    after removing files not needed

--- app/src/main/java/com/radiostationslist/activity/MainActivity.java
+++ app/src/main/java/com/radiostationslist/activity/MainActivity.java
@@ -42,7 +42,8 @@ public class MainActivity extends Activity implements OnReceiveServerResponse {
     private ArrayList<StationModel> al_FM_StationData = new ArrayList<>();
     private ArrayList<StationModel> al_InternetRadio_StationData = new ArrayList<>();
 
-    private String strUrlToRequest = "http://phorus.vtuner.com/setupapp/phorus/asp/browsexml/navXML.asp?gofile=LocationLevelFourCityUS-North%20America-New%20York-ExtraDir-1-Inner-14&bkLvl=9237&mac=a8f58cd9758b710c43a7a63762e755947f83f0ad9194aa294bbaee55e0509e02&dlang=eng&fver=1.4.4.2299%20(20150604)&hw=CAP:%201.4.0.075%20MCU:%201.032%20BT:%200.002";
+    private String strUrlToRequest =
+            "http://phorus.vtuner.com/setupapp/phorus/asp/browsexml/navXML.asp?gofile=LocationLevelFourCityUS-North%20America-New%20York-ExtraDir-1-Inner-14&bkLvl=9237&mac=a8f58cd9758b710c43a7a63762e755947f83f0ad9194aa294bbaee55e0509e02&dlang=eng&fver=1.4.4.2299%20(20150604)&hw=CAP:%201.4.0.075%20MCU:%201.032%20BT:%200.002";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
